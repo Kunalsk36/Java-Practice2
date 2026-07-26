93258bbf012080874e89457c01b34a8895f8c79c
Kunalsk36 <kunalkavathekar@gmail.com>
2026-07-26 09:28:13 +0530

    Prime Number

---
+++ PrimeNumber.java
@@ -0,0 +1,32 @@
+// 4. Prime Number
+
+// Check whether a number is prime.
+
+// Input:
+// 29
+
+// Output:
+// Prime
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static boolean isPrime(int num){
+        if(num <= 1){
+            return false;
+        }
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println(isPrime(num));
+    }
+}
