b8b49468af84cedbbef5b7aa41e09b98f927b618
Kunalsk36 <kunalkavathekar@gmail.com>
2026-07-26 11:09:04 +0530

    ArmStrong Number

---
+++ ArmStrongNumber.java
@@ -0,0 +1,37 @@
+// 8. Armstrong Number
+// Input:
+// 153
+
+// Output:  
+// Armstrong
+
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        if(num < 0){
+            System.out.println("Not a Armstrong Number");
+            sc.close();
+            return;
+        }
+        long power = String.valueOf(num).length();
+        double sum = 0;
+        int temp = num;
+        while(temp != 0){
+            int digit = temp % 10;
+            sum = sum + Math.pow(digit, power);
+            temp /= 10;
+        }
+        if(num == sum){
+            System.out.println("Armstrong number");
+        }
+        else{
+            System.out.println("Not Armstrong number");
+        }
+        sc.close();
+    }
+}
