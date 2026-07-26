9377206c81019fc9499e34394f97e2da0e12e435
Kunalsk36 <kunalkavathekar@gmail.com>
2026-07-26 10:58:54 +0530

    Sum of Digits of number

---
+++ SumOfDigits.java
@@ -0,0 +1,25 @@
+// 7. Sum of Digits
+// Input:
+// 456
+
+// Output:
+// 15
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int resultSum = 0;
+        int temp = num; 
+        while(temp != 0){
+            int digit = temp % 10;
+            resultSum += digit;
+            temp /= 10;
+        } 
+        System.out.println("Sum of Digits of "+num+" is "+resultSum);
+        sc.close();
+    }
+}
