d068e24e53b52e6d10f43fde3a10ec0e1a113297
Kunalsk36 <kunalkavathekar@gmail.com>
2026-07-26 08:54:04 +0530

    Even Odd Program

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+// 1. Even or Odd
+
+// Input an integer and print whether it is Even or Odd.
+
+// Example
+
+// Input: 7
+// Output: Odd
+
+import java.util.Scanner;
+
+class EvenOdd{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println("Number is even");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+    }
+}
