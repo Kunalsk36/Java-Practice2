38cec8ea71591f0355af6ab47077da88b3a27d0e
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-03 22:52:22 +0530

    Palindrom number check

---
+++ PalindromeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class PalindromeNumber{
+    public boolean isPalindrome(int x){
+        if (x < 0){
+            return false;
+        }              
+        int original = x;
+        int reversed = 0;
+        while (x > 0){
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        PalindromeNumber palindrome = new PalindromeNumber();
+        if (palindrome.isPalindrome(num)){
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+    }
+}
