5a566ea1070ee7d2b339129197b19228c099ad1f
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-07 23:21:08 +0530

    Largest element in array

---
+++ LargestElArr.java
@@ -0,0 +1,12 @@
+public class LargestElArr {
+    public static void main(String[] args){
+        int[] arr = {10, 2, 8, 4, 5};
+        int largest = Integer.MIN_VALUE;
+        for(int i: arr){
+            if(i > largest){
+                largest = i;
+            }
+        }
+        System.out.println(largest);
+    }
+}
