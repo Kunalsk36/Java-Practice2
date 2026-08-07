864088bad0934a6659c715dc3059758d79743213
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-08 00:37:46 +0530

    Print Pattern 1

---
+++ PrintPattern1.java
@@ -0,0 +1,11 @@
+public class PrintPattern1 {
+    public static void main(String[] args){
+        String star = "*";
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(star);
+            }
+            System.out.println();
+        }
+    }
+}
