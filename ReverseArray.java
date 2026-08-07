86884bc03e1a3f737b082663f48bb286e1fc8f4f
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-08 00:09:36 +0530

    Reverse Array

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int size = arr.length;
+        int n = 0;
+        int[] revArr = new int[size];
+        for(int i = size - 1; i >= 0; i--){
+            revArr[n] = arr[i];
+            n++;
+        }
+        for(int i: revArr){
+            System.out.print(i+ " ");
+        }
+    }
+}
