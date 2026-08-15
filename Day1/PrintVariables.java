d2afd46687dff00f3dd06c5da1a124a81282d384
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-15 20:07:20 +0530

    Declare and Print Variables

---
+++ Day1/PrintVariables.java
@@ -0,0 +1,19 @@
+// Declare int, double, char, boolean and String variables and print them.
+
+package Day1;
+
+public class PrintVariables {
+    public static void main(String[] args) {
+        int num = 36;
+        double cgpa = 8.9;
+        char ch = 'K';
+        boolean isActive = true;
+        String name = "Kunal";
+
+        System.out.println(num);
+        System.out.println(cgpa);
+        System.out.println(ch);
+        System.out.println(isActive);
+        System.out.println(name);
+    }
+}
