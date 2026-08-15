a8ccce4fd7e0d68b1e3c67187386e28b0bc4600a
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-15 20:46:07 +0530

    Swap nums using 3 variables

---
+++ Day2/SwapNumsUsing3Var.java
@@ -0,0 +1,21 @@
+// Swap two numbers using a third variable.
+
+package Day2;
+
+import java.util.Scanner;
+
+public class SwapNumsUsing3Var {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1 : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num2 : ");
+        int num2 = sc.nextInt();
+        System.out.println("Before Swap : num1 = "+num1+" and num2 = "+num2);
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After Swap : num1 = "+num1+" and num2 = "+num2);
+        sc.close();
+    }
+}
