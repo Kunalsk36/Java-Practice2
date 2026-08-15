fb061253da6f9a65bbb69a7d943527e49b274e17
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-15 21:04:17 +0530

    Swap two nums without 3rd variable

---
+++ Day2/SwapNums.java
@@ -0,0 +1,21 @@
+// Swap two numbers without using a third variable.
+
+package Day2;
+
+import java.util.Scanner;
+
+public class SwapNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num2: ");
+        int num2 = sc.nextInt();
+        System.out.println("Before Swapping: num1 = "+num1+" and num2 = "+num2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("After Swapping: num1 = "+num1+" and num2 = "+num2);
+        sc.close();
+    }
+}
