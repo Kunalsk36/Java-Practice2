3572ad1b241009a8f973530cd7dbe12e9b78a7c1
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-15 20:02:53 +0530

    Print sentences using multiple println statements

---
+++ Day1/PrintSentences.java
@@ -0,0 +1,11 @@
+// Print a sentence using multiple System.out.println() statements.
+
+package Day1;
+
+public class PrintSentences {
+    public static void main(String[] args) {
+        System.out.println("Hello, ");
+        System.out.println("I am Kunal Kavathekar");
+        System.out.println("I am from Ratnagiri.");
+    }
+}
