39d4911bdb5f7e06b5a849362cb2c6936e6d1bcb
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-16 20:38:46 +0530

    Calculate Total and Average of 3 Numbers

---
+++ Day2/TotalAndAverage.java
@@ -0,0 +1,24 @@
+// Calculate total and average of three numbers.
+
+package Day2;
+
+import java.util.Scanner;
+
+public class TotalAndAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total;
+        double avg;
+        System.out.println("Enter num1 : ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter num2 : ");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter num3 : ");
+        double num3 = sc.nextDouble();
+        total = num1+num2+num3;
+        avg = ( num1 + num2 + num3 ) / 3;
+        System.out.println("The total is "+total);
+        System.out.println("The average is "+avg);
+        sc.close();
+    }
+}
