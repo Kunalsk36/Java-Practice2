7cbb03c2d8cb4f88ed3570f1feb56db514110ba7
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-16 20:45:23 +0530

    Celsius to Fahrenheit

---
+++ Day2/CtoF.java
@@ -0,0 +1,16 @@
+// Convert Celsius to Fahrenheit.
+
+package Day2;
+
+import java.util.Scanner;
+
+public class CtoF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Celsius: ");
+        double celsius = sc.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Celsius to Fahrenheit is "+fahrenheit);
+        sc.close();
+    }
+}
