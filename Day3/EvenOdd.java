0fb34fbda54a6366ed7c3a551e2f7ad6dd7e3d7f
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-16 20:58:48 +0530

    Even Odd Number

---
+++ Day3/EvenOdd.java
@@ -0,0 +1,20 @@
+// Check whether a number is even or odd.
+
+package Day3;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println("The num "+num+" is Even.");
+        }
+        else{
+            System.out.println("The num "+num+" is Odd.");
+        }
+        sc.close();
+    }
+}
