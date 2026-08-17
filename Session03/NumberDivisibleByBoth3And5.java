1daee411f0e635a718b417c3fc7c1160d37f9688
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-17 12:19:14 +0530

    Number is divisible by 3 and 5

---
+++ Session03/NumberDivisibleByBoth3And5.java
@@ -0,0 +1,24 @@
+package Session03;
+
+import java.util.Scanner;
+
+public class NumberDivisibleByBoth3And5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        if(num % 3 == 0 && num % 5 == 0){
+            System.out.println("The number is divisible by both 3 and 5.");
+        }
+        else if(num % 3 == 0){
+            System.out.println("The number is divisible by 3.");
+        }
+        else if(num % 5 == 0){
+            System.out.println("The number is divisible by 5");
+        }
+        else{
+            System.out.println("The number is not divisible by both 3 and 5");
+        }
+        sc.close();
+    }
+}
