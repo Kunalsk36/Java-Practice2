58f4b54c1817d69d2bce4bd07fa18ed5b0f41815
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-21 15:47:55 +0530

    Lear Year Checker

---
+++ Session05/LeapYearChecker.java
@@ -0,0 +1,20 @@
+// Check whether a year is a leap year.
+
+package Session05;
+
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year : ");
+        int year = sc.nextInt();
+        if( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ) {
+            System.out.println("The entered year is Leap Year.");
+        }
+        else {
+            System.out.println("The entered year is Not a Leap Year.");
+        }
+        sc.close();
+    }
+}
