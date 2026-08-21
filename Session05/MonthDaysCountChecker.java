6367a4ae66306a8380a1f12db8c0a284ab6875cc
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-21 22:39:38 +0530

    Month Days Count Checker

--- Session05/MonthDaysCountChecker.java
+++ Session05/MonthDaysCountChecker.java
@@ -15,8 +15,74 @@
 
 package Session05;
 
+import java.util.Scanner;
+
 public class MonthDaysCountChecker {
     public static void main(String[] args) {
+        // Method 1 - Switch
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the month: (1-12) : ");
+        int month = sc.nextInt();
+        System.out.println("Enter the year : ");
+        int year = sc.nextInt();
+        int days = 0;
+        if(year <= 0) {
+            System.out.println("Entered wrong year!");
+        }
+        else {
+            switch (month) {
+                case 1:
+                    days = 31;
+                    break;
+                case 2:
+                    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                        days = 29;
+                    }
+                    else {
+                        days = 28;
+                    }
+                    break;
+                case 3:
+                    days = 31;
+                    break;
+                case 4:
+                    days = 30;
+                    break;
+                case 5:
+                    days = 31;
+                    break;
+                case 6: 
+                    days = 30;
+                    break;
+                case 7:
+                    days = 31;
+                    break;
+                case 8:
+                    days = 31;
+                    break;
+                case 9:
+                    days = 30;
+                    break;
+                case 10:
+                    days = 31;
+                    break;
+                case 11:
+                    days = 30;
+                    break;
+                case 12:
+                    days = 31;
+                    break;
+            
+                default:
+                    System.out.println("Entered wrong month!");
+                    return;
+            }
+            System.out.println("The month has "+days+" days!");
+        }
+
+
+        // Method 2:
+        /*
         int[] d_31 = {1, 3, 5, 7, 8, 10, 12};
         int[] d_30 = {4, 6, 9, 11};
         int feb = 2;
@@ -49,5 +115,6 @@ public class MonthDaysCountChecker {
                 }
             }
         }
+        */
     }
 }
