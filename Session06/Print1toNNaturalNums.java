7730e9b1888f4c2fff8a1835566745b74ca4ffab
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-23 10:51:20 +0530

    Print 1 to N natural nums

---
+++ Session06/Print1toNNaturalNums.java
@@ -0,0 +1,18 @@
+// Print the first N natural numbers.
+
+package Session06;
+
+import java.util.Scanner;
+
+public class Print1toNNaturalNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number N : ");
+        int n = sc.nextInt();
+        System.out.println("The Natural numbers from 1 to "+n+" : ");
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
