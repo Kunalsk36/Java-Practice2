2c8ba7a7e7497dca2097345c8d2b5e19fd55124d
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-23 10:26:33 +0530

    Print numbers from starting and ending number inclusive

---
+++ Session06/PrintInclusiveNums.java
@@ -0,0 +1,23 @@
+package Session06;
+
+import java.util.Scanner;
+
+public class PrintInclusiveNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the start number : ");
+        int startN = sc.nextInt();
+        System.out.println("Enter the end number : ");
+        int endN = sc.nextInt();
+        if(startN>endN){
+            System.out.println("Invalid Input : Entered ending number greater than starting number.");
+        }
+        else{
+            System.out.println("The Inclusive numbers from "+startN+" to "+endN+" :");
+            for(int i = startN; i<=endN;i++){
+                System.out.println(i); 
+            }
+        }
+        sc.close();
+    }
+}
