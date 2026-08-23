3cb2b9a884fd3f8780e92b543cded67efa56f2cd
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-23 10:10:20 +0530

    Print 1 to N odd numbers

---
+++ Session06/Print1toNOddNums.java
@@ -0,0 +1,18 @@
+package Session06;
+
+import java.util.Scanner;
+
+public class Print1toNOddNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N Number: ");
+        int n = sc.nextInt();
+        System.out.println("\nFollowing are the Odd numbers from 1 to "+n+" : ");
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
