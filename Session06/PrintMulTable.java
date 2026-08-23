0d3adf3908188ae2c508507a1341e1c00f8244d3
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-23 10:34:50 +0530

    Print the multiplication table of a number

---
+++ Session06/PrintMulTable.java
@@ -0,0 +1,18 @@
+// Print the multiplication table of a number.
+
+package Session06;
+
+import java.util.Scanner;
+
+public class PrintMulTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Multiplication Table of "+n+" : ");
+        for(int i = 1; i<=10;i++) {
+            System.out.println(i*n);
+        }
+        sc.close();
+    }
+}
