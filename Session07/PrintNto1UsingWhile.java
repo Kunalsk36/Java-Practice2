59caf1b26ab6cc0e15b37d951572d0e1bf65a46a
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-24 11:20:16 +0530

    Print N to 1 using While Loop

---
+++ Session07/PrintNto1UsingWhile.java
@@ -0,0 +1,18 @@
+package Session07;
+
+import java.util.Scanner;
+
+public class PrintNto1UsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the N Number : ");
+        int n = sc.nextInt();
+        int i = n;
+        System.out.println("The Numbers from "+n+" to 1 are : ");
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+        sc.close();
+    }
+}
