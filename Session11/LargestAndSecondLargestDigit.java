f6957ef8c238d7cfa4a2e62f76b2f3c2d6d27fb4
Kunalsk36 <kunalkavathekar@gmail.com>
2026-08-28 11:58:25 +0530

    Largest and Second Largest Digit from number

---
+++ Session11/LargestAndSecondLargestDigit.java
@@ -0,0 +1,35 @@
+// Find the largest and second largest digit.
+
+package Session11;
+
+import java.util.Scanner;
+
+public class LargestAndSecondLargestDigit {
+    public static int[] largestAndSecondLargestDigit(int num) {
+        int largestDigit = -1;
+        int secondLargestDigit = -1;
+        int temp = Math.abs(num);
+        if(num == 0) {
+            return new int[] {0 , -1};
+        }
+        while(temp > 0) {
+            int digit = temp % 10;
+            if(digit > largestDigit) {
+                secondLargestDigit = largestDigit;
+                largestDigit = digit;
+            }
+            else if(digit > secondLargestDigit && digit < largestDigit) {
+                secondLargestDigit = digit;
+            }
+            temp /= 10;
+        }
+        return new int[] {largestDigit, secondLargestDigit};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        int[] res = largestAndSecondLargestDigit(num);
+        System.out.println("From the number "+num+" the largest digit is "+res[0]+" and second largest digit is "+res[1]);
+    }
+}
