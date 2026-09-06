934149aafc393117b7b4d5cd7c983f3433fdc8df
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 11:39:54 +0530

    Print Pattern 2

---
+++ Session14/PrintPattern2.java
@@ -0,0 +1,25 @@
+/* Print Pattern:
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+5 5 5 5 5
+*/
+
+package Session14;
+
+import java.util.Scanner;
+
+public class PrintPattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
