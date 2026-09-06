a26f566493bb88ea0f097b2cc9abfe09c97c0b48
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 12:30:10 +0530

    Print pattern filled triangle

---
+++ Session14/PrintPattern9.java
@@ -0,0 +1,33 @@
+/* Print Pattern:
+
+*         
+* *       
+* * *     
+* * * *   
+* * * * * 
+
+*/
+
+package Session14;
+
+import java.util.Scanner;
+
+public class PrintPattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n value: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(j <= i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
