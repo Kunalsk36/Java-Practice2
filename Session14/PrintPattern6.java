fb0322a6f0c72c2081be2f99cd4cf1d1488895ad
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 12:08:15 +0530

    Print Pattern - reverse diagonal

---
+++ Session14/PrintPattern6.java
@@ -0,0 +1,33 @@
+/* Print Pattern - reverse diagonal
+
+        * 
+      *   
+    *     
+  *       
+*         
+
+*/
+
+package Session14;
+
+import java.util.Scanner;
+
+public class PrintPattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i+j == n+1) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
