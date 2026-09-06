643096d6028719168eda22f4bb71145a1fdb6b32
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 11:51:21 +0530

    Print Pattern - Hollow square

---
+++ Session14/PrintPattern4.java
@@ -0,0 +1,33 @@
+/* Print Pattern: Hollow Square
+
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
+
+*/
+
+package Session14;
+
+import java.util.Scanner;
+
+public class PrintPattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number n : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
