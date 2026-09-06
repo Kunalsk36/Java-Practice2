eb26fd6542e1cd08f255496e3e1d1edf74e23ef2
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 11:46:07 +0530

    Print solid pattern

--- Session14/PrintPattern3.java
+++ Session14/PrintPattern3.java
@@ -1,22 +1,25 @@
-/*  Print:
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
+/* Print Pattern: Solid 
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
 */
+
 package Session14;
 
 import java.util.Scanner;
 
-public class PrintPattern1 {
+public class PrintPattern3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the n value : ");
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                System.out.print(j+" ");
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
