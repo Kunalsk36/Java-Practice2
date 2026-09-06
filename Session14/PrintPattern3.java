2e8171e2ffb63a1aa955857e974e5e23353ace12
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-06 11:52:16 +0530

    fix the pattern

--- Session14/PrintPattern3.java
+++ Session14/PrintPattern3.java
@@ -17,8 +17,8 @@ public class PrintPattern3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the n value : ");
         int n = sc.nextInt();
-        for(int i = 1; i <= 5; i++) {
-            for(int j = 1; j <= 5; j++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
