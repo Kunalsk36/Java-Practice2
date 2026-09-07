2f783a6572a12929947f7ff74bd1d9783ef73ffc
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-07 12:10:51 +0530

    Print pattern tringle

---
+++ Session14/PrintPattern11.java
@@ -0,0 +1,37 @@
+package Session14;
+
+import java.util.Scanner;
+
+public class PrintPattern11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value :");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n * 2 - 1; j++) {
+                if (i + j == n + 1 || j == n - 1 + i || (j >= n + 1 - i && j <= n - 1 + i)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+/*
+ * 3 -->
+ *
+ ***
+ *****
+ * 
+ * 5 -->
+ *
+ ***
+ *****
+ *******
+ *********
+ * 
+ * 
+ */
