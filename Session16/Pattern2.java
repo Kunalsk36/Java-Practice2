75a024f6206ccd812bc5fc3d891d66549c64bc02
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-09 11:25:46 +0530

    Print Pattern 2

--- Session16/Pattern2.java
+++ Session16/Pattern2.java
@@ -1,10 +1,14 @@
-/* Print pattern: 
+/* Print Pattern : 
 
-    *     
-    *     
+*         
+* *       
+* * *     
+* * * *   
 * * * * * 
-    *     
-    *     
+* * * *   
+* * *     
+* *       
+* 
 
 */
 
@@ -12,19 +16,20 @@ package Session16;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the n value : ");
+        System.out.print("Enter the n value : ");
         int n = sc.nextInt();
-        for(int i = 1; i <=n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(i==n/2+1 || j==n/2+1) {
+        for(int i = 1; i <= n*2-1; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i==j||i+j==n*2 || (j<=i && i+j <=n*2)) {
                     System.out.print("* ");
                 }
                 else{
                     System.out.print("  ");
                 }
+                // System.out.print("("+i+","+j+")");
             }
             System.out.println();
         }
