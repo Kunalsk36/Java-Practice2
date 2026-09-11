1942f3289ce8145bfb2a0f2ec3ed14d2c1600c73
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-11 11:21:54 +0530

    Print Pattern 7

---
+++ Session16/Pattern7.java
@@ -0,0 +1,37 @@
+/* Print Pattern : 
+
+* * * * * 
+* * * *   
+* * *     
+* *       
+*         
+* *       
+* * *     
+* * * *   
+* * * * *
+
+*/
+
+package Session16;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n*2-1; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i+j<=n+1 || j+n-1 <= i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                    // System.out.print("("+i+","+j+")");
+            }
+            System.out.println();
+        }
+    }
+}
