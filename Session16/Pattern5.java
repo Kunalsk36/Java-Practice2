91a225f0a6be5982acb3b5af6446bad1be125ada
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-11 10:51:06 +0530

    Print Pattern

---
+++ Session16/Pattern5.java
@@ -0,0 +1,33 @@
+/* Print Pattern : 
+
+*               * 
+* *           * * 
+* * *       * * * 
+* * * *   * * * * 
+* * * * * * * * *
+
+*/
+
+package Session16;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n*2-1; j++) {
+                if(i+j >= n*2 || i >= j) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                    // System.out.print("("+i+","+j+")");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
