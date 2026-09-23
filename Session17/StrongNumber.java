523d442f871ccf737c0851e4c8e674ae82fbf56c
Kunalsk36 <kunalkavathekar@gmail.com>
2026-09-23 11:30:36 +0530

    Strong number explanation

--- Session17/StrongNumber.java
+++ Session17/StrongNumber.java
@@ -1,3 +1,9 @@
+/*
+A strong number (also known as a Krishnamurthy number or special number) is a number where the sum of the factorials of its digits is equal to the original number itself.
+
+For example, 145 is a strong number because: (1!+4!+5! = 1+24+120=145)
+*/
+
 package Session17;
 
 import java.util.Scanner;
